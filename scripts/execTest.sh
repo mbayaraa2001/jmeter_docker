10feac06c8cd3d937325b54485c99b8dddea781e
mbayaraa2001 <mbayaraa2001@yahoo.com>
2022-12-01 13:47:25 +0800

    results are saved

--- scripts/execTest.sh
+++ scripts/execTest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#eval "docker stop "
 #
-SLAVE_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep slave | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
-eval "docker exec -it -d jmeterdocker-slave-1 /bin/bash -c '/jmeter/bin/jmeter-server -Djava.rmi.server.hostname=$SLAVE_IP'"
+#SLAVE_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep slave | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
+#eval "docker exec -it -d jmeterdocker-slave-1 /bin/bash -c '/jmeter/bin/jmeter-server -Djava.rmi.server.hostname=$SLAVE_IP'"
 
 
-mkdir $NAME && 
+#mkdir $NAME && 
